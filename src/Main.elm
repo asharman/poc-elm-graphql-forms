54287cea18b13119f0717ddb40747de1e016a80a
Austin Sharman <asharman@generacgs.com>
2023-08-25 16:14:58 -0600

    hook up a query

--- src/Main.elm
+++ src/Main.elm
@@ -1,54 +1,81 @@
 module Main exposing (Msg(..), main)
 
 import Browser
+import Graphql.Http exposing (Request)
+import Graphql.Operation exposing (RootQuery)
+import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
 import Html exposing (Html)
 import Html.Attributes as Attributes
-import Html.Events as Events
+import PlaceholderApi.Object.User as User
+import PlaceholderApi.Object.UsersPage as UsersPage
+import PlaceholderApi.Query as Query
+
+
+
+-- API
+
+
+query : SelectionSet a RootQuery -> Request a
+query selectionSet =
+    Graphql.Http.queryRequest "https://graphqlzero.almansi.me/api" selectionSet
+
+
+
+-- MODEL
 
 
 type alias Model =
-    { count : Int
+    { users : List String
     }
+
+
+
+-- LIFECYCLE
+
+
+type Msg
+    = QueryResponse (Result (Graphql.Http.Error (Maybe (List String))) (Maybe (List String)))
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { count = 0 }, Cmd.none )
+    let
+        userSelection =
+            User.name
+                |> SelectionSet.nonNullOrFail
+
+        usersPageSelection =
+            UsersPage.data userSelection
+                |> SelectionSet.nonNullOrFail
+                |> SelectionSet.nonNullElementsOrFail
+
+        usersQuery =
+            Query.users (\a -> always a ()) usersPageSelection
+    in
+    ( { users = [] }, Graphql.Http.send QueryResponse (query usersQuery) )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        QueryResponse (Ok maybeUsers) ->
+            ( { model | users = maybeUsers |> Maybe.withDefault [] }, Cmd.none )
+
+        QueryResponse (Err _) ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
     Html.div [ Attributes.class "flex flex-col items-center" ]
         [ Html.h1 [ Attributes.class "text-4xl font-bold text-blue-500" ] [ Html.text "Vite and Elm starter" ]
-        , Html.div [ Attributes.class "flex flex-col items-center" ]
-            [ Html.button
-                [ Events.onClick Increment
-                , Attributes.class "border px-8 hover:bg-slate-200"
-                ]
-                [ Html.text "+1" ]
-            , Html.div [] [ Html.text (String.fromInt model.count) ]
-            , Html.button
-                [ Events.onClick Decrement
-                , Attributes.class "border px-8 hover:bg-slate-200"
-                ]
-                [ Html.text "-1" ]
-            ]
+        , Html.div [ Attributes.class "flex flex-col items-center" ] <|
+            List.map (\user -> Html.p [] [ Html.text user ]) model.users
         ]
-
-
-type Msg
-    = Increment
-    | Decrement
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Increment ->
-            ( { model | count = model.count + 1 }, Cmd.none )
-
-        Decrement ->
-            ( { model | count = model.count - 1 }, Cmd.none )
 
 
 main : Program () Model Msg
